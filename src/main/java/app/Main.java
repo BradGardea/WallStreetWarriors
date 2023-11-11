2ccb04a2b093c86519faa80e25fb5ebb9bc7d8a6
Brad G <brad.gardea@gmail.com>
2023-11-11 13:32:20 -0500

    Added sample run for testing firestore read and write

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,36 +1,75 @@
 package app;
 
+import FirebaseDataAccess.FirebaseDataAccess;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 import interface_adapters.Contests.ContestViewModel;
 import interface_adapters.ViewModelManager;
 import view.ContestView;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
 
 class Main{
     public static void main(String[] args){
-        JFrame app = new JFrame("Wall Street Warriors");
-        app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        CardLayout cardLayout = new CardLayout();
+        Firestore db;
+        //Initialization for firebase.
+        try{
+            URL url =  Main.class.getResource("/wallstreetwarriors-firebase-adminsdk-8g503-9cad46c515.json");
+            File file = new File(url.getPath());
 
-        JPanel views = new JPanel(cardLayout);
-        app.add(views);
+            FileInputStream serviceAccount =
+                    new FileInputStream(file);
 
-        ViewModelManager viewModelManager = new ViewModelManager();
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
 
-        ContestViewModel contestViewModel = new ContestViewModel();
-        ContestView contestView = ContestUseCaseFactory.create(contestViewModel);
+            FirebaseApp.initializeApp(options);
 
-        views.add(contestView, contestView.viewName);
+            db = FirestoreClient.getFirestore();
 
-        viewModelManager.setActiveView(contestView.viewName);
-        viewModelManager.firePropertyChanged();
+            //"Initialize" singleton entity level data access factory
+            var firebaseDataAccess = FirebaseDataAccess.getInstance();
+            firebaseDataAccess.setFirestore(db);
 
-        app.pack();
-        app.setVisible(true);
+            firebaseDataAccess.getEntity(Message.class, "Messages", "123");
+            Message m = new Message("too", "loo");
+
+            firebaseDataAccess.setOrUpdateEntity(m, "Messages", "1234");
 
 
+            JFrame app = new JFrame("Wall Street Warriors");
+            app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+            CardLayout cardLayout = new CardLayout();
+
+            JPanel views = new JPanel(cardLayout);
+            app.add(views);
+
+            ViewModelManager viewModelManager = new ViewModelManager();
+
+            ContestViewModel contestViewModel = new ContestViewModel();
+            ContestView contestView = ContestUseCaseFactory.create(contestViewModel);
+
+            views.add(contestView, contestView.viewName);
+
+            viewModelManager.setActiveView(contestView.viewName);
+            viewModelManager.firePropertyChanged();
+
+            app.pack();
+            app.setVisible(true);
+        }
+        catch (Exception ex){
+            System.out.println("Unable to load Firebase data, app will have limited functionality.");
+        }
     }
 
 }
