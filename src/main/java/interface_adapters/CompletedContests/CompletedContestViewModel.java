fa56df0529a5f6d5dca0fc0e0847cbeb78a87ca6
dhruvpatt <84606623+dhruvpatt@users.noreply.github.com>
2023-11-13 15:31:29 -0500

    added scaffolding for completedcontestviewmodel

--- src/main/java/interface_adapters/CompletedContests/CompletedContestViewModel.java
+++ src/main/java/interface_adapters/CompletedContests/CompletedContestViewModel.java
@@ -3,6 +3,10 @@ package interface_adapters.CompletedContests;
 import view.CompletedContests.CompletedContestView;
 
 public class CompletedContestViewModel {
+
+
+
+
     public void addPropertyChangeListener(CompletedContestView completedContestsView) {
     }
 }
