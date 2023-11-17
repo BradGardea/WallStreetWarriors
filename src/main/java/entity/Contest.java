b77df6b4ff9313cc4e31f7406831faedb84ffee9
dhruvpatt <84606623+dhruvpatt@users.noreply.github.com>
2023-11-17 15:45:06 -0500

    refactored contests

--- src/main/java/entity/Contest.java
+++ src/main/java/entity/Contest.java
@@ -1,18 +1,73 @@
 package entity;
 
+import FirebaseDataAccess.FirebaseDataAccess;
+import FirebaseDataAccess.IFirebaseEntity;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.UUID;
 
-public interface Contest {
+public class StockContest implements Contest, IFirebaseEntity {
 
-    String getTitle();
+    private final String contestId = UUID.randomUUID().toString();
+    private String title;
 
-    String getDescription();
+    private String description;
 
-    ArrayList<User> getMembers();
+    private ArrayList<String> members;
 
-    String getContestId();
+    private String industry;
 
-    String getContestIndustry();
+    private LocalDateTime startTime;
 
-    User getWinner();
+    private LocalDateTime endTime;
+
+    public String getTitle(){ return this.title; }
+
+    public String getDescription(){ return this.description; }
+
+    public ArrayList<User> getMembers(){
+        var users = new ArrayList<User>();
+        var dataAccess = FirebaseDataAccess.getInstance();
+        for(var userId: this.members){
+            users.add(dataAccess.getEntity(User.class, "Users", userId));
+        }
+        return users;
+    }
+
+    public String getContestId(){ return this.contestId; }
+
+    public String getContestIndustry(){ return this.industry; }
+
+
+
+    //TODO: Implement Method Later when API call logic is finished
+    public User getWinner(){ return null; }
+
+    public StockContest(String title, String description, ArrayList<String> members,
+                        String industry, LocalDateTime startTime, LocalDateTime endTime){
+//        this.contestId = contestId;
+        this.title = title;
+        this.description = description;
+        this.members = members;
+        this.industry = industry;
+        this.startTime = startTime;
+        this.endTime = endTime;
+
+    }
+
+    @Override
+    public void GetDocument(String collection, String iD) {
+
+    }
+
+    @Override
+    public void SetOrUpdateDocument(String collection, String iD, IFirebaseEntity currObject) {
+
+    }
+
+    @Override
+    public void DeleteDocument(String collection, String iD) {
+
+    }
 }
